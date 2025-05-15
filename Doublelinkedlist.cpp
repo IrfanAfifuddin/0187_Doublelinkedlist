562f8a0bf47526ba656ed7302352533d36a86626
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-05-15 19:46:35 +0700

    step 3

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -113,5 +113,9 @@ class DoubleLinkedList
             if (START != NULL)
                 START->prev = NULL; // Step 2b: START.prev = NULL
         }
+        else
+        {
+            // Step 3: Link previous node to next of current
+            current->prev->next = current->next;
     }
 };
