80f29a6f3bc0aa5b93cf890a32b4b06498312334
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-05-15 19:46:22 +0700

    step 1

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -95,5 +95,15 @@ class DoubleLinkedList
         cin >> rollNo;
 
         Node *current = START;
+
+        // Step 1: Traverse the list to find the node
+        while (current != NULL && current->noMhs != rollNo)
+            current = current->next;
+
+        if (current == NULL)
+        {
+            cout << "Record not found" << endl;
+            return;
+        }
     }
 };
