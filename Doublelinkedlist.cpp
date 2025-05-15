6e4867f73e7ae84c011fef1ef7f19b92826231bb
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-05-15 19:43:51 +0700

    step 1

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -27,5 +27,7 @@ class DoubleLinkedList
         string nm;
         cout << "\nEnter the roll number of the student: ";
         cin >> nim;
+        // Step 1: Allocate memory for new node
+        Node* newNode = new Node();
     }   
 };
